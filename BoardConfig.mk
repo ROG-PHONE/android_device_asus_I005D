8bf0bb762de4c6a30853f788a15aee04f63ed9cc
nguyenhung9xdev <93065362+nguyenhung9xdev@users.noreply.github.com>
2022-02-07 11:18:34 +0700

    I005D: initial device tree

---
+++ BoardConfig.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from asus sm8350-common
+-include device/asus/sm8350-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/asus/I005D
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
+
+# Kernel
+TARGET_KERNEL_CONFIG := vendor/I005D_defconfig
+
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+
+# inherit from the proprietary version
+-include vendor/asus/I005D/BoardConfigVendor.mk
